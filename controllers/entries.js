7462373ef2009f82b07700513257152f918d0468
oceankay2426 <travelkay24@gmail.com>
2024-04-22 15:44:43 -0500

    commit

--- controllers/entries.js
+++ controllers/entries.js
@@ -27,6 +27,8 @@ async function index(req, res) {
     month = today.getMonth();
   }
   const daysInMo = new Date(year, month + 1, 0).getDate();
-  const entries = await Entry.find({date: {$gte: `${year}-${month}-01T00:00`}, date: {$lte: `${year}-${daysInMo}-01T23:59`}});
+  const startDate = new Date(year, month, 1);
+  const endDate = new Date(year, month, daysInMo);
+  const entries = await Entry.find({date: {$gte: startDate, $lte: endDate}});
   res.render('entries/index', { title: 'MONTHLY LOG', entries, year, month, monthNames });
 };
--- routes/entries.js
+++ routes/entries.js
@@ -4,35 +4,7 @@ const entriesCtrl = require('../controllers/entries');
 	
 //all path start with /entries
 
-const monthNames = [
-    'January', 'February', 'March', 'April',
-    'May', 'June', 'July', 'August',
-    'September', 'October', 'November', 'December',
-  ];
-  
-  router.get('/calendar', function(req, res, next) {
-    let year = parseInt(req.query.year);
-    let month = parseInt(req.query.month);
-    if (month === -1) {
-      year--;
-      month = 11;
-    } else if (month === 12) {
-      year++;
-      month = 0;
-    }
-  
-    // Default to today's year and month if not specified
-    if (!year) {
-      const today = new Date();
-      year = today.getFullYear();
-      month = today.getMonth();
-    }
-  
-    const daysInMo = new Date(year, month + 1, 0).getDate();
-    const dow = new Date(year, month, 1).getDay() + 1;
-  
-    res.render('entries/index', { year, month, monthNames, daysInMo, dow, title: 'fit'}); 
-  });
+
 // GET /entries (index function)
 router.get('/', entriesCtrl.index);
 	
