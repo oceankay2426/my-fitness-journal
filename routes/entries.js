2aff70b25f72f44445286e8b9f92dc11e72d92cd
oceankay2426 <travelkay24@gmail.com>
2024-04-25 11:47:30 -0500

    commit

--- routes/entries.js
+++ routes/entries.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const entriesCtrl = require('../controllers/entries');
-	
+
 //all path start with /entries
 router.post('/', entriesCtrl.create);
 // GET /entries (index function)
